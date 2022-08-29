2541218e2f9ad6b2f85dcb40f08fab0f1686bf3f
dray-the-sea <109749397+dray-the-sea@users.noreply.github.com>
2022-08-29 11:15:30 -0500

    added file creation when app is run for the first time

--- Sampler-v3/Data/ModelData.swift
+++ Sampler-v3/Data/ModelData.swift
@@ -9,46 +9,65 @@ import Foundation
 import Combine
 
 final class ModelData: ObservableObject {
+    @Published var observations: [Observation] = load(filename: "Observations.json")
     
-    @Published var observations: [Observation] = load()
- 
     func save() {
-      guard let dataURL = getURL() else {
-          fatalError("Error retrieving URL where data should be saved. ")
-      }
+        
+        
+        guard let dataURL = getURL() else {
+            fatalError("Error retrieving URL where data should be saved. ")
+        }
         do {
-          let data = try JSONEncoder().encode(observations)
-          try data.write(to: dataURL, options: .atomic)
+            
+            let filepath = dataURL.appendingPathComponent("Observations.json")
+            
+            let data = try JSONEncoder().encode(observations)
+            try data.write(to: filepath, options: .atomic)
         } catch {
             fatalError("Error writing to JSON file: \(error)")
         }
     }
     
-
+    
 }
 
 
 func getURL() -> URL? {
-  guard let documentsURL = FileManager.default.urls(
-    for: .documentDirectory, in: .userDomainMask).first else {
-    return nil
-  }
-  return documentsURL.appendingPathComponent("Observations.json")
+    guard let documentsURL = FileManager.default.urls(
+        for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+    return documentsURL
 }
 
 
-func load<T: Decodable>() -> T{
+func load<T: Decodable>(filename: String) -> T{
     let data: Data
     
     guard let dataURL = getURL() else {
         fatalError("Error retrieving URL where data should be saved. ")
     }
-
+    let filepath = dataURL.appendingPathComponent(filename)
+    
+    let fileManager = FileManager.default
+    if !fileManager.fileExists(atPath: filepath.path) {
+        
+        do {
+            let emptyArray: [Observation] = []
+            let data = try JSONEncoder().encode(emptyArray)
+            try data.write(to: filepath, options: .atomic)
+        } catch {
+            fatalError("Couldn't create a new Observations file")
+        }
+    }
+    
     do {
-      data = try Data(contentsOf: dataURL)
-      
+        data = try Data(contentsOf: filepath)
+        
     } catch {
-        fatalError("Couldn't load contents of \(dataURL) from main bundle: \n \(error)")
+        // if i change the contents of the Feeling enum, this will come into play. Fix later
+        // TO DO: fix this later
+        fatalError("Couldn't load contents of \(filename) from documents directory: \n \(error)")
     }
     
     
@@ -61,31 +80,3 @@ func load<T: Decodable>() -> T{
         fatalError("Couldn't parse \(dataURL) as \(T.self): \n\(error) ")
     }
 }
-
-/*
-func load<T: Decodable>(_ filename: String) -> T {
-    let data: Data
-    
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else{
-        fatalError("Couldn't find \(filename) in main bundle.")
-    }
-    
-    do {
-        data = try Data(contentsOf: file)
-    }
-    catch {
-        fatalError("Couldn't load contents of \(filename) from main bundle: \n \(error)")
-    }
-    
-    do {
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    }
-    catch
-    {
-        fatalError("Couldn't parse \(filename) as \(T.self): \n\(error) ")
-    }
-}
- */
-
