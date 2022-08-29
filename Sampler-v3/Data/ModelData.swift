698d5c12384935d387e5f7054a7709976ddcb058
dray-the-sea <109749397+dray-the-sea@users.noreply.github.com>
2022-08-29 10:37:40 -0500

    saves to file in the app's documents directory

--- Sampler-v3/Data/ModelData.swift
+++ Sampler-v3/Data/ModelData.swift
@@ -10,25 +10,59 @@ import Combine
 
 final class ModelData: ObservableObject {
     
-    @Published var observations: [Observation] = load("myObservations.json")
-    
+    @Published var observations: [Observation] = load()
+ 
     func save() {
-        let filename: String = "myObservations.json"
-        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-        else{
-            fatalError("Couldn't find \(filename) in main bundle.")
-        }
-                
+      guard let dataURL = getURL() else {
+          fatalError("Error retrieving URL where data should be saved. ")
+      }
         do {
-            let jsonData = try JSONEncoder().encode(observations)
-            try jsonData.write(to: file)
+          let data = try JSONEncoder().encode(observations)
+          try data.write(to: dataURL, options: .atomic)
         } catch {
             fatalError("Error writing to JSON file: \(error)")
         }
     }
+    
+
 }
 
 
+func getURL() -> URL? {
+  guard let documentsURL = FileManager.default.urls(
+    for: .documentDirectory, in: .userDomainMask).first else {
+    return nil
+  }
+  return documentsURL.appendingPathComponent("Observations.json")
+}
+
+
+func load<T: Decodable>() -> T{
+    let data: Data
+    
+    guard let dataURL = getURL() else {
+        fatalError("Error retrieving URL where data should be saved. ")
+    }
+
+    do {
+      data = try Data(contentsOf: dataURL)
+      
+    } catch {
+        fatalError("Couldn't load contents of \(dataURL) from main bundle: \n \(error)")
+    }
+    
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }
+    catch
+    {
+        fatalError("Couldn't parse \(dataURL) as \(T.self): \n\(error) ")
+    }
+}
+
+/*
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
     
@@ -53,4 +87,5 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self): \n\(error) ")
     }
 }
+ */
 
